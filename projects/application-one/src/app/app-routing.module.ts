75ba9f3e7db76c35b90b92472217530adb5904a9
riefive <muhammad.rifai@salt.co.id>
2023-12-19 15:13:59 +0700

    feat(application1): add page home + about

--- projects/application-one/src/app/app-routing.module.ts
+++ projects/application-one/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
